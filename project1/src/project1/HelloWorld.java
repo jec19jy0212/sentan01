50c09df7c930baa6c5e7e80fe38257ab46777f71
19jy0212 <19jy0212@PC40.jynet2.jec.ac.jp>
2020-11-12 14:14:43 +0900

    mod 1～10の中でランダムに繰り返すように変更

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -3,9 +3,12 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-		System.out.println("Java");
-		System.out.println("HTML");
+		int rand = (int)(Math.random() * 10) + 1;
+		for(int i=0; i<rand; i++) {
+			System.out.println("HelloWorld");
+			System.out.println("Java");
+			System.out.println("HTML");
+		}
 	}
 
 }
