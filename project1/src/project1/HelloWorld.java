858c911b44b28ea429bf5fbbe9a45e108c664d9a
19jy0212 <19jy0212@PC40.jynet2.jec.ac.jp>
2020-11-12 14:11:46 +0900

    mod 5回繰り返して表示するように変更

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -3,9 +3,11 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-		System.out.println("Java");
-		System.out.println("HTML");
+		for(int i=0; i<5; i++) {
+			System.out.println("HelloWorld");
+			System.out.println("Java");
+			System.out.println("HTML");
+		}
 	}
 
 }
